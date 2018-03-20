221d5e3377c222b93a0a0d278a0b4c6804e38ee7
IamCharlesM <because@charlesis.me>
2018-03-20 12:45:11 -0500

    Page designed, bootstrap installed, "jobs" loading on page.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
