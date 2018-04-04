263c7d0b843157a59bd7c7c927ed31a431fdd81f
IamCharlesM <because@charlesis.me>
2018-04-04 09:33:19 -0500

    added title to current jobs, added text below jumbotron.

--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.CharField(max_length=255)
+    title = models.CharField(max_length=100, default='Job Title')
