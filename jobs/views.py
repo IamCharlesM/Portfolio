f01b283e1f1bf669fcfe117e651106ba7a6ef09a
IamCharlesM <because@charlesis.me>
2018-04-04 21:48:28 -0500

    Added link to desktop project, linked css files, linked js file.

--- jobs/views.py
+++ jobs/views.py
@@ -4,3 +4,5 @@ from .models import Job
 def home(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs':jobs})
+def computer(request):
+    return render(request, 'My-computer/index.html')
