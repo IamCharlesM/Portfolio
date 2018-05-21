7ca4fdb82fc09323368c4f4c1ea1392aea5db8d9
iamcharlesm <because@charlesis.me>
2018-05-21 10:31:11 -0500

    Job cards now extend from home page ate /blog.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
+from jobs.models import Job
 
 def allblogs(request):
     blogs = Blog.objects
-    return render(request, 'blog/allblogs.html', {'blogs':blogs})
+    jobs = Job.objects
+    return render(request, 'blog/allblogs.html', {'blogs':blogs, 'jobs':jobs})
 
 def detail(request, blog_id):
     detailblog = get_object_or_404(Blog, pk=blog_id)
